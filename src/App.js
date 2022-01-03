e1278b6f3afeb54819e58945e923e9303401b3cc
Brett Schellenberg <brett.schellenberg@gmail.com>
2022-01-03 12:45:01 -0700

    basic clickable grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Fragment, useState} from 'react';
 import './App.css';
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const handleSquareClick = (e, rowIndex, colIndex, setGrid) => {
+  console.log("e", e)
+  console.log("row", rowIndex)
+  console.log("col", colIndex)
+  setGrid(prevGrid => {
+    const gridCopy = _.cloneDeep(prevGrid);
+    gridCopy[rowIndex][colIndex] = !gridCopy[rowIndex][colIndex];
+    console.log("setting grid to ", gridCopy)
+    return gridCopy;
+  });
 }
 
-export default App;
+const Square = ({enabled, rowIndex, colIndex, setGrid}) => {
+  return <div
+    className={enabled ? 'square-enabled' : 'square'}
+    onClick={(e) => handleSquareClick(e, rowIndex, colIndex, setGrid)}></div>
+}
+
+const Row = ({rowData, rowIndex, setGrid}) => {
+  return <div>
+    {rowData.map((data, colIndex) => {
+      return <Square setGrid={setGrid} rowIndex={rowIndex} colIndex={colIndex} enabled={!!data} />
+    })}
+  </div>
+}
+
+const Grid = () => {
+  const [grid, setGrid] = useState([
+    [0,0,0,0,0],
+    [0,0,1,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]
+  ]);
+
+  console.log("grid rendering and state is ", grid)
+
+  return <div>
+    {grid.map((row, rowIndex) => {
+      return <Row setGrid={setGrid} rowData={row} rowIndex={rowIndex} />
+    })}
+  </div>
+}
+
+export const App = () => {
+  return <div>
+    Here's what's up
+    <Grid />
+  </div>
+}
