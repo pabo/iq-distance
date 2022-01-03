f38f10ca28a602587cdd62cc59d129e7b182092d
Brett Schellenberg <brett.schellenberg@gmail.com>
2022-01-03 14:43:06 -0700

    init gh-pages

--- src/App.js
+++ src/App.js
@@ -1,55 +1,134 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import _ from 'lodash';
 
-
 const handleSquareClick = (e, rowIndex, colIndex, setGrid) => {
-  console.log("e", e)
-  console.log("row", rowIndex)
-  console.log("col", colIndex)
   setGrid(prevGrid => {
     const gridCopy = _.cloneDeep(prevGrid);
     gridCopy[rowIndex][colIndex] = !gridCopy[rowIndex][colIndex];
-    console.log("setting grid to ", gridCopy)
     return gridCopy;
   });
 }
 
-const Square = ({enabled, rowIndex, colIndex, setGrid}) => {
-  return <div
-    className={enabled ? 'square-enabled' : 'square'}
-    onClick={(e) => handleSquareClick(e, rowIndex, colIndex, setGrid)}></div>
+const calculateDistanceGrid = (grid) => {
+  const distanceGrid = _.cloneDeep(grid); // lazy way to get the right size grid
+  let minDistance = 255;
+  let maxDistance = 0;
+
+  grid.forEach((row, rowIndex)  => {
+      row.forEach((square, colIndex) => {
+
+        let distance = 0;
+
+        grid.forEach((gridRow, gridRowIndex)  => {
+          gridRow.forEach((gridSquare, gridSquareIndex) => {
+            if (gridSquare) {
+              const rowDiff = gridRowIndex - rowIndex;
+              const colDiff = gridSquareIndex - colIndex;
+              const hypotenuse = Math.sqrt(rowDiff*rowDiff + colDiff*colDiff);
+              distance += hypotenuse;
+            }
+          })
+        })
+
+        const truncDistance = parseFloat(distance.toFixed(2));
+
+        distanceGrid[rowIndex][colIndex] = truncDistance;
+
+        // only set max and min if this square isnt selected
+        if (!square) {
+          maxDistance = Math.max(truncDistance, maxDistance);
+          minDistance = Math.min(truncDistance, minDistance);
+        }
+    })
+  })
+
+  return {
+    distanceGrid,
+    minDistance,
+    maxDistance
+  };
+
 }
 
-const Row = ({rowData, rowIndex, setGrid}) => {
-  return <div>
+const Square = ({enabled, rowIndex, colIndex, setGrid, distances}) => {
+  const {maxDistance, minDistance, distanceGrid} = distances;
+  const thisDistance = distanceGrid[rowIndex][colIndex];
+
+  const zeroedMax = maxDistance - minDistance;
+  const zeroedThis = thisDistance - minDistance;
+  const percentThis = zeroedThis / zeroedMax;
+  const colorValue = 255 - 255 * percentThis;
+  const colorString = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
+
+  const classNames = `
+    ${enabled ? 'square-enabled' : 'square'} 
+    ${thisDistance === minDistance ? 'minimum' : ''}
+  `
+
+  return <div
+    className={classNames}
+    onClick={(e) => handleSquareClick(e, rowIndex, colIndex, setGrid)}
+    style={{"background-color": colorString}}>
+      { thisDistance }
+    </div>
+}
+
+const Row = ({rowData, rowIndex, grid, setGrid, distances}) => {
+  return <div className="flexDiv">
     {rowData.map((data, colIndex) => {
-      return <Square setGrid={setGrid} rowIndex={rowIndex} colIndex={colIndex} enabled={!!data} />
+      return <Square distances={distances} setGrid={setGrid} grid={grid} rowIndex={rowIndex} colIndex={colIndex} enabled={!!data} />
     })}
   </div>
 }
 
 const Grid = () => {
-  const [grid, setGrid] = useState([
-    [0,0,0,0,0],
-    [0,0,1,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0]
-  ]);
+  // const [grid, setGrid] = useState([
+  //   [0,0,0],
+  //   [0,0,0],
+  //   [0,0,0],
+  // ]);
 
-  console.log("grid rendering and state is ", grid)
+  const emptyGrid = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  ];
+
+  const [grid, setGrid] = useState(emptyGrid);
+  const distances = calculateDistanceGrid(grid);
+
+  // const {maxDistance, minDistance, distanceGrid} = distances;
 
   return <div>
     {grid.map((row, rowIndex) => {
-      return <Row setGrid={setGrid} rowData={row} rowIndex={rowIndex} />
+      return <Row setGrid={setGrid} grid={grid} distances={distances} rowData={row} rowIndex={rowIndex} />
     })}
+    <button onClick={() => setGrid(emptyGrid)}>Reset Grid</button>
   </div>
 }
 
 export const App = () => {
   return <div>
-    Here's what's up
     <Grid />
   </div>
 }
